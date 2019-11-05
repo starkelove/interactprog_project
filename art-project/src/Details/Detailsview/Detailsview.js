1915c72f161582a998db1cad64eb22f5ddd2faf5
Sara Ersson <saraers@kth.se>
2019-11-05 13:12:59 +0100

    fixed image

--- art-project/src/Details/Detailsview/Detailsview.js
+++ art-project/src/Details/Detailsview/Detailsview.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Detailsview.css";
 import Topview from "../../Topview/Topview";
+import pooop from "./pooop.png";
 
 class Detailview extends Component {
 
@@ -35,14 +36,16 @@ class Detailview extends Component {
     // create variables
     let title = null;
     let description = null;
+    let image = null;
 
     switch(this.state.status) {
       case "LOADING":
         break;
 
       case "LOADED":
-      title = "Item Title";
+        title = "Item Title";
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        image = pooop;
         break;
 
       default:
@@ -60,7 +63,7 @@ class Detailview extends Component {
             <p>{description}</p>
             </div>
             <div className="col-sm-6">
-            <img id="item-image" src="pooop.png"></img>
+            <img id="item-image" src={image}></img>
             </div>
         </div>
         <button id="add-to-chart" className="btn btn-secondary" onClick={this.handleAdd}>Add to chart</button>
