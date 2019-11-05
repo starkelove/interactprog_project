51b34c0f7185c6669a3105d303a033b8287a4d1c
Sara Ersson <saraers@kth.se>
2019-11-05 14:18:06 +0100

    linked to /main

---
+++ art-project/src/Main/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Main.css";
+import Topview from "../Topview/Topview";
+import ItemView from "../ItemView/ItemView";
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="Main">
+                <Topview></Topview>
+                <ItemView></ItemView>
+            </div>
+        );
+    }
+}
+
+export default Main;
