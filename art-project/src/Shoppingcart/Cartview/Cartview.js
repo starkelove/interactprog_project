96b2a13a768d307822c989b61deb5050e8fb8a36
Anna Eklind <annaekli@kth.se>
2019-11-13 12:43:51 +0100

    paypal up and running

--- art-project/src/Shoppingcart/Cartview/Cartview.js
+++ art-project/src/Shoppingcart/Cartview/Cartview.js
@@ -1,18 +1,8 @@
 import React, { Component } from "react";
 import "./Cartview.css";
 import { base } from "../../base";
-//import PaypalButton from "./PaypalButton";
 import {PayPalButton} from "react-paypal-button-v2"
 
-/* const CLIENT = {
-  sandbox : process.env.PAYPAL_CLIENT_ID_SANDBOX,
-  production : process.env.PAYPAL_CLIENT_ID_PRODUCTION,
-}
-
-const ENV = process.env.NODE_ENV === 'production'
-  ? 'production'
-  : 'sandbox';
- */
 class Cartview extends Component {
   constructor(props) {
     super(props);
@@ -49,15 +39,6 @@ class Cartview extends Component {
         }
       });*/
     
-    /* const onSuccess = (payment) =>
-      console.log("Successful payement ", payment);
-
-    const onError = (error) => 
-      console.log("Errorneous payment OR failed to load script ", error);
-
-    const onCancel = (data) =>
-      console.log("Cancelled payment ", data); */
-
     return (
 
      <div className="Cartview">
@@ -71,37 +52,29 @@ class Cartview extends Component {
                 </div>
                 <div className="col">
                 <PayPalButton
-                  amount="0.01"
-                  onSuccess = {(details) => 
-                    alert("Transaction completed by " + details.payer.name.given_name)}
-                  /* onSuccess = {(details, data) => {
-                    alert("Transaction completed by " + details.payer.name.given_name);
-
-                    return fetch("/paypal-transaction-complete", {
-                      method: "post",
-                      body: JSON.stringify({
-                        orderId: data.orderId
-                      })
+                  createOrder={(data, actions) => { 
+                    return actions.order.create({
+                      purchase_units: [{
+                        amount: {
+                          value: "0.01"
+                        }
+                      }],
                     });
                   }}
+                  onSuccess = {(details) => 
+                    alert("Transaction completed by " + details.payer.name.given_name)} 
+                  onError = {(error) => 
+                    alert(error)}
+                  onCancel = {(data) => 
+                    alert("The transaction was cancelled ")
+                  }
                   options={{
-                    clientId: process.env.PAYPAL_CLIENT_ID_SANDBOX
-                  }} */
+                    clientId: "sb",
+                    currency: "SEK"
+                  }}
+                  
                 />  
-                {/* <button id="confirm-buy" className="btn btn-secondary" onClick={this.handleOrder}>Place order!</button> */}
-               {/*  <PaypalButton
-                  client = {CLIENT}
-                  env = {ENV}
-                  commit = {true}
-                  currency = {'USD'}
-                  total = {100}
-                  onSuccess = {onSuccess}
-                  onError = {onError}
-                  onCancel = {onCancel}
-                /> */}
-                </div>
-                
-                
+                </div>   
             </div>
     </div>
     );
