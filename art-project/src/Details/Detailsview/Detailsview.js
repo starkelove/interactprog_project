c2441ef759e0e284e27c15b18ce7c7f484c82532
Anna Eklind <annaekli@kth.se>
2019-11-20 10:53:07 +0100

    gets current product id

--- art-project/src/Details/Detailsview/Detailsview.js
+++ art-project/src/Details/Detailsview/Detailsview.js
@@ -10,8 +10,14 @@ class Detailview extends Component {
   constructor(props) {
         super(props);
 
+        let productId = window.location.href;
+        let arr = productId.split("/")
+        productId = arr[arr.length-1]
+    
         this.state = {
-            status: "LOADING"
+            status: "LOADING",
+            selectedProduct: productId,
+            item : []
         };
     }
 
@@ -23,9 +29,20 @@ class Detailview extends Component {
     // remove observer
   }
 
+  // this methods is called by React lifecycle when the
+  // component is actually shown to the user (mounted to DOM)
+  // that's a good place to call the API and get the data
   componentDidMount() {
-    // add observer
-    this.setState({status: "LOADED" });
+    // when data is retrieved we update the state
+    // this will cause the component to re-render
+
+     // add observer
+    this.setState({
+      status: "LOADED",
+      title: "Linocut Thumbelina",
+      description: "As a kid I had a pillowcase with a print of a wild grown summers field. It had tall grass, dandelions, lily of the valley and it made me feel like I was so small I could live under the flowers. When I had a daughter I wanted to give her something similar, so I created this Thumbelina-inspired linocut to inspire dreams of adventure and safety.",
+      image: img 
+    });
   }
 
   update() {
@@ -35,18 +52,19 @@ class Detailview extends Component {
 
   render() {
     // create variables
-    let title = null;
-    let description = null;
-    let image = null;
+    let title = "";
+    let description = "";
+    let image = "";
 
     switch(this.state.status) {
       case "LOADING":
+        title = "Loading...";
         break;
 
       case "LOADED":
-        title = "Linocut Thumbelina";
-        description = "As a kid I had a pillowcase with a print of a wild grown summers field. It had tall grass, dandelions, lily of the valley and it made me feel like I was so small I could live under the flowers. When I had a daughter I wanted to give her something similar, so I created this Thumbelina-inspired linocut to inspire dreams of adventure and safety.";
-        image = img;
+        title = this.state.title;
+        description = this.state.description;
+        image = this.state.image;
         break;
 
       default:
