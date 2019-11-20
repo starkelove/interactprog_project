d1cc3ff743cb494d0e711ff6f8e0ee2dff0149fb
Sara Ersson <saraers@kth.se>
2019-11-20 12:36:18 +0100

    detailsview fetches item and calls addToCart in model

--- art-project/src/Details/Detailsview/Detailsview.js
+++ art-project/src/Details/Detailsview/Detailsview.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import "./Detailsview.css";
 import Topview from "../../Topview/Topview";
 import img from "../../imgs/thumbelina/thumbelina1.jpg";
+import model from "../../Data/Model";
+import { base } from '../../base'
 
 
 class Detailview extends Component {
@@ -13,26 +15,54 @@ class Detailview extends Component {
         let productId = window.location.href;
         let arr = productId.split("/")
         productId = arr[arr.length-1]
-    
+
         this.state = {
             status: "LOADING",
             selectedProduct: productId,
-            item : []
         };
     }
 
-  handleAdd = (event) => {
-    console.log("Add to chart");
+  handleAdd = () => {
+    console.log("handleAdd");
+    console.log(this.state.item);
+    if(this.state.item != undefined) {
+      model.addToCart(this.state.item);
+    }
   }
 
   componentWillUnmount() {
     // remove observer
   }
 
+  componentDidMount(){
+
+  }
   // this methods is called by React lifecycle when the
   // component is actually shown to the user (mounted to DOM)
   // that's a good place to call the API and get the data
   componentDidMount() {
+    base.bindToState(`products/${this.state.selectedProduct}`, {
+      context: this,
+      state: "item",
+      asArray: false
+    });
+
+    model
+    .getItem(this.state.selectedProduct)
+    .then(result => {
+      this.setState({
+        status: "LOADED",
+        item: result,
+      });
+      console.log("Here is result");
+      console.log(this.state.item);
+    })
+    .catch(() => {
+      this.setState({
+        status: "ERROR"
+      });
+    });
+
     // when data is retrieved we update the state
     // this will cause the component to re-render
 
@@ -41,7 +71,7 @@ class Detailview extends Component {
       status: "LOADED",
       title: "Linocut Thumbelina",
       description: "As a kid I had a pillowcase with a print of a wild grown summers field. It had tall grass, dandelions, lily of the valley and it made me feel like I was so small I could live under the flowers. When I had a daughter I wanted to give her something similar, so I created this Thumbelina-inspired linocut to inspire dreams of adventure and safety.",
-      image: img 
+      image: img
     });
   }
 
